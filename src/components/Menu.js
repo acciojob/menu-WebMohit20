f92f8155de38aee00ca8daeda2427f0dfe881eba
WebMohit20 <2003mohitkhemani@gmail.com>
2024-12-02 13:10:35 +0000

    functional menu2

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,9 +7,9 @@ const  Menu=({setSortDish})=>{
             <h1 className="title">Our Menu</h1>
             <div className="menu-nav" >
                 <button id="main" onClick={()=> setSortDish("all") } > All </button>
-                <button id="filter-btn-1" onClick={()=> setSortDish("breakfast") } > Breakfast</button>
-                <button id="filter-btn-2" onClick={()=> setSortDish("lunch") } > Lunch </button>
-                <button id="filter-btn-3" onClick={()=> setSortDish("shakes") } > Shakes </button>
+                <button id="filter-btn-1" data-test-id="menu-item-breakfast" onClick={()=> setSortDish("breakfast") } > Breakfast</button>
+                <button id="filter-btn-2" data-test-id="menu-item-lunch" onClick={()=> setSortDish("lunch") } > Lunch </button>
+                <button id="filter-btn-3" data-test-id="menu-item-shakes" onClick={()=> setSortDish("shakes") } > Shakes </button>
             </div>
         </div>
     )
