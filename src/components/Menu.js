c1b9f5908b4244e33cb15bde771cde704a88a707
WebMohit20 <2003mohitkhemani@gmail.com>
2024-12-10 16:33:52 +0000

    commit2

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,7 +7,7 @@ const  Menu=({setSortDish})=>{
             <h1 className="title">Our Menu</h1>
             <div className="menu-nav" >
                 <button id="main" onClick={()=> setSortDish("all") } > All </button>
-                <button id="filter-btn-2" data-test-id="menu-item-breakfast" onClick={()=> setSortDish("breakfast") } > Breakfast</button>
+                <button id="filter-btn-1" data-test-id="menu-item-breakfast" onClick={()=> setSortDish("breakfast") } > Breakfast</button>
                 <button id="filter-btn-2" data-test-id="menu-item-lunch" onClick={()=> setSortDish("lunch") } > Shakes </button>
                 <button id="filter-btn-3" data-test-id="menu-item-shakes" onClick={()=> setSortDish("shakes") } > Lunch </button>
             </div>
